3430689f65292c58deca50e3f870801455f105fe
Ryan Persée <ryan.persee@synchrotron-soleil.fr>
2022-07-18 17:58:25 +0200

    Fixes `pam_sm_setcred` issue on Ubuntu 22.04
    ```
    ... xrdp-sesman[768]: PAM unable to resolve symbol: pam_sm_setcred
    ```

--- src/pam_visa.c
+++ src/pam_visa.c
@@ -49,3 +49,8 @@ int pam_sm_authenticate(pam_handle_t * pamh, int flags, int argc, const char **a
 
     return PAM_IGNORE;
 }
+
+PAM_EXTERN
+int pam_sm_setcred(pam_handle_t * pamh, int flags, int argc, const char **argv) {
+    return PAM_SUCCESS;
+}
